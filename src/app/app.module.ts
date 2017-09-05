d7048110fccee79a2ea9c9a3fe53ac910c7e2eec
DirkViljoen <dirkcharl.viljoen@gmail.com>
2017-09-05 07:09:48 +0200

    feat (angular material): add angular material for theming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MdListModule,
+  MdIconModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { ListsComponent } from './components/lists/lists.component';
@@ -10,7 +16,9 @@ import { ListsComponent } from './components/lists/lists.component';
     ListsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdListModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
